d6660a9fd4fc98a313dcf32c878e0dd9b9e53780
Сергей Королев <sergejkorolev@192.168.1.7>
2022-10-02 19:53:09 +0300

    AddConnectionToDatabase

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,14 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseNpgsql(connection);
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
