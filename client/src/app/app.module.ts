665498d032df404752256571077380701008320b
Сергей Королев <sergejkorolev@192.168.1.3>
2022-10-13 15:59:37 +0300

    AddRegister

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     AuthComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
